d0b0ee199e3798a05cc0bfb72966f329f2439e49
47sinnha <126620648+47sinnha@users.noreply.github.com>
2025-07-31 19:59:19 +0530

    Fix: added homepage and base for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/gate-prep-app/",
-  plugins: [react()]
-});
+  base: '/gate-prep-app/', // 👈 this should match your repo name
+  plugins: [react()],
+})
